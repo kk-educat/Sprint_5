89b99c65459773f19e8bf06f265f274c3b16d068
Konstantin K <for.educat.etc@gmail.com>
2026-08-06 18:36:45 +0300

    united tests into classes

--- tests/test_exit.py
+++ tests/test_exit.py
@@ -5,17 +5,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 import locators
 
 
-def test_access_to_constructor_through_link(driver):
-    # входим в аккаунт
-    driver.get("https://stellarburgers.education-services.ru/login")
-    driver.find_element(By.XPATH, locators.SIGN_IN_MAIL_FIELD).send_keys('kostya_kozhanov_49@domen.org')
-    driver.find_element(By.XPATH, locators.SIGN_IN_PSWD_FIELD).send_keys('e7Yhgt')
-    driver.find_element(By.XPATH, locators.SIGN_IN_BUTTON).click()
-    WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, locators.CREATE_ORDER_BUTTON)))
-    # переходим в личный кабинет
-    driver.find_element(By.XPATH, locators.PERSONAL_CABINET_BUTTON).click()
-    WebDriverWait(driver, 5).until(expected_conditions.url_to_be('https://stellarburgers.education-services.ru/account/profile'))
-    # проверяем выход по кнопке Выйти
-    driver.find_element(By.XPATH, locators.EXIT_BUTTON).click()
-    WebDriverWait(driver, 5).until(expected_conditions.url_to_be('https://stellarburgers.education-services.ru/login'))
-    assert driver.current_url == 'https://stellarburgers.education-services.ru/login'
+class TestExit:
+    def test_access_to_constructor_through_link(self, driver):
+        # входим в аккаунт
+        driver.get("https://stellarburgers.education-services.ru/login")
+        driver.find_element(By.XPATH, locators.SIGN_IN_MAIL_FIELD).send_keys('kostya_kozhanov_49@domen.org')
+        driver.find_element(By.XPATH, locators.SIGN_IN_PSWD_FIELD).send_keys('e7Yhgt')
+        driver.find_element(By.XPATH, locators.SIGN_IN_BUTTON).click()
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, locators.CREATE_ORDER_BUTTON)))
+        # переходим в личный кабинет
+        driver.find_element(By.XPATH, locators.PERSONAL_CABINET_BUTTON).click()
+        WebDriverWait(driver, 5).until(expected_conditions.url_to_be('https://stellarburgers.education-services.ru/account/profile'))
+        # проверяем выход по кнопке Выйти
+        driver.find_element(By.XPATH, locators.EXIT_BUTTON).click()
+        WebDriverWait(driver, 5).until(expected_conditions.url_to_be('https://stellarburgers.education-services.ru/login'))
+        assert driver.current_url == 'https://stellarburgers.education-services.ru/login'
--- tests/test_sign_up.py
+++ tests/test_sign_up.py
@@ -6,26 +6,27 @@ from generator import generate_login
 import locators
 
 
-def test_registration_success(driver):
-    test_pswd = 'e7Yhgt'
-    test_user_name = 'Konstantin'
-    test_email = generate_login()
-    driver.get("https://stellarburgers.education-services.ru/register")
-    driver.find_element(By.XPATH, locators.REGISTRATION_NAME_FIELD).send_keys(test_user_name)
-    driver.find_element(By.XPATH, locators.REGISTRATION_MAIL_FIELD).send_keys(test_email)
-    driver.find_element(By.XPATH, locators.REGISTRATION_PSWD_FIELD).send_keys(test_pswd)
-    driver.find_element(By.XPATH, locators.REGISTRATION_BUTTON).click()
-    WebDriverWait(driver, 5).until(expected_conditions.url_to_be('https://stellarburgers.education-services.ru/login'))
-    assert driver.current_url == 'https://stellarburgers.education-services.ru/login'
+class TestSignUp:
+    def test_registration_success(self, driver):
+        test_pswd = 'e7Yhgt'
+        test_user_name = 'Konstantin'
+        test_email = generate_login()
+        driver.get("https://stellarburgers.education-services.ru/register")
+        driver.find_element(By.XPATH, locators.REGISTRATION_NAME_FIELD).send_keys(test_user_name)
+        driver.find_element(By.XPATH, locators.REGISTRATION_MAIL_FIELD).send_keys(test_email)
+        driver.find_element(By.XPATH, locators.REGISTRATION_PSWD_FIELD).send_keys(test_pswd)
+        driver.find_element(By.XPATH, locators.REGISTRATION_BUTTON).click()
+        WebDriverWait(driver, 5).until(expected_conditions.url_to_be('https://stellarburgers.education-services.ru/login'))
+        assert driver.current_url == 'https://stellarburgers.education-services.ru/login'
 
-def test_registration_error(driver):
-    test_pswd = 'e7Yhg' # некорректный пароль длиной меньше 6 символов
-    test_user_name = 'Konstantin'
-    test_email = generate_login()
-    driver.get("https://stellarburgers.education-services.ru/register")
-    driver.find_element(By.XPATH, locators.REGISTRATION_NAME_FIELD).send_keys(test_user_name)
-    driver.find_element(By.XPATH, locators.REGISTRATION_MAIL_FIELD).send_keys(test_email)
-    driver.find_element(By.XPATH, locators.REGISTRATION_PSWD_FIELD).send_keys(test_pswd)
-    driver.find_element(By.XPATH, locators.REGISTRATION_BUTTON).click()
-    error = WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, locators.REGISTRATION_WRONG_PSWD_MSG)))
-    assert error.text == "Некорректный пароль"
+    def test_registration_error(self, driver):
+        test_pswd = 'e7Yhg' # некорректный пароль длиной меньше 6 символов
+        test_user_name = 'Konstantin'
+        test_email = generate_login()
+        driver.get("https://stellarburgers.education-services.ru/register")
+        driver.find_element(By.XPATH, locators.REGISTRATION_NAME_FIELD).send_keys(test_user_name)
+        driver.find_element(By.XPATH, locators.REGISTRATION_MAIL_FIELD).send_keys(test_email)
+        driver.find_element(By.XPATH, locators.REGISTRATION_PSWD_FIELD).send_keys(test_pswd)
+        driver.find_element(By.XPATH, locators.REGISTRATION_BUTTON).click()
+        error = WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, locators.REGISTRATION_WRONG_PSWD_MSG)))
+        assert error.text == "Некорректный пароль"
